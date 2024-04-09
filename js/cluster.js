ab183b5d37ceff10d7e5e1b805adb8c20121813d
lzyfly <136085166+zjhubio@users.noreply.github.com>
2024-04-09 13:35:12 +0800

    Update cluster.js

--- js/cluster.js
+++ js/cluster.js
@@ -6,54 +6,37 @@ async function cluster(){
 	var selectElement = document.getElementById('trainingset');
     var selectedValue = selectElement.value;
 	
-	const client = new OSS({
-       region: 'oss-cn-beijing',
-       bucket: "darsep",
-    });
-	
 	if(selectedValue == 1){
-		var img1 = "1.png";
-		var img2 = "2.png";
-		alert("请求已发送，请等待！")
-		await sleep(10000);
-		client.get(img1).then(function(result) {
-			var imageUrl = URL.createObjectURL(new Blob([result.content]));
-			document.getElementById('img1').src = imageUrl;
-			document.getElementById('annotation1').innerHTML = "Semantic embedding clustering plot for the training sequence dataset";
-		}).catch(function(err) {
-		  console.error('Error downloading image:', err);
-		});
+		var img1 = "./images/1.png";
+		var img2 = "./images/2.png";
+		alert("The request has been sent, please wait！")
+		await sleep(1000);
+	
+		document.getElementById('img1').src = img1;
+		document.getElementById('annotation1').innerHTML = "Semantic embedding clustering plot for the training sequence dataset";
 		
-		client.get(img2).then(function(result) {
-			var imageUrl = URL.createObjectURL(new Blob([result.content]));
-			document.getElementById('img2').src = imageUrl;
-			document.getElementById('annotation2').innerHTML = "Semantic embedding clustering plot for the training sequence dataset, coloured by year";
-		}).catch(function(err) {
-		  console.error('Error downloading image:', err);
-		});
+		
+			
+		document.getElementById('img2').src = img2;
+		document.getElementById('annotation2').innerHTML = "Semantic embedding clustering plot for the training sequence dataset, coloured by year";
+
 		document.getElementById('clusterimgs').style.display = 'block';
 	}
 	
 	if(selectedValue == 2){
-		var img3 = "3.png";
-		var img4 = "4.png";
-		alert("请求已发送，请等待！")
-		await sleep(10000);
-		client.get(img3).then(function(result) {
-			var imageUrl = URL.createObjectURL(new Blob([result.content]));
-			document.getElementById('img1').src = imageUrl;
-			document.getElementById('annotation1').innerHTML = "Semantic embedding clustering plot for the training vs. optimised dataset";
-		}).catch(function(err) {
-		  console.error('Error downloading image:', err);
-		});
+		var img3 = "./images/3.png";
+		var img4 = "./images/4.png";
+		alert("The request has been sent, please wait！")
+		await sleep(1000);
 		
-		client.get(img4).then(function(result) {
-			var imageUrl = URL.createObjectURL(new Blob([result.content]));
-			document.getElementById('img2').src = imageUrl;
-			document.getElementById('annotation2').innerHTML = "Semantic embedding clustering plot for the training vs. optimised dataset, coloured by year";
-		}).catch(function(err) {
-		  console.error('Error downloading image:', err);
-		});
+		document.getElementById('img1').src = img3;
+		document.getElementById('annotation1').innerHTML = "Semantic embedding clustering plot for the training vs. optimised dataset";
+
+		
+
+		document.getElementById('img2').src = img4;
+		document.getElementById('annotation2').innerHTML = "Semantic embedding clustering plot for the training vs. optimised dataset, coloured by year";
+
 		document.getElementById('clusterimgs').style.display = 'block';
 	}
 }
