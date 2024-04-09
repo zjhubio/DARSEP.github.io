cc6973bd21cacb0251af28ffb567f9c54aa304c3
lzyfly <136085166+zjhubio@users.noreply.github.com>
2024-04-09 13:36:19 +0800

    Update vocsanalysis.js

--- js/vocsanalysis.js
+++ js/vocsanalysis.js
@@ -6,73 +6,49 @@ async function vocsanalysis(){
 	var selectElement = document.getElementById('vocs');
     var selectedValue = selectElement.value;
 	
-	const client = new OSS({
-       region: 'oss-cn-beijing',
-       bucket: "darsep",
-    });
 	
 	if(selectedValue == 5){
-		var img3 = "11.png";
-		var img4 = "12.png";
-		var img5 = "13.png";
-		alert("请求已发送，请等待！")
+		var img3 = "./images/11.png";
+		var img4 = "./images/12.png";
+		var img5 = "./images/13.png";
+		alert("The request has been sent, please wait！")
 		await sleep(10000);
-		client.get(img3).then(function(result) {
-			var imageUrl = URL.createObjectURL(new Blob([result.content]));
-			document.getElementById('img6').src = imageUrl;
-			document.getElementById('annotation6').innerHTML = "The mutation location of the RBD region of the Omicron variable strain BA.1.";
-		}).catch(function(err) {
-		  console.error('Error downloading image:', err);
-		});
+
+		document.getElementById('img6').src = img3;
+		document.getElementById('annotation6').innerHTML = "The mutation location of the RBD region of the Omicron variable strain BA.1.";
 		
-		client.get(img4).then(function(result) {
-			var imageUrl = URL.createObjectURL(new Blob([result.content]));
-			document.getElementById('img7').src = imageUrl;
-			document.getElementById('annotation7').innerHTML = "Semantic variations and percentage rankings of mutation positions in the RBD region of the Omicron variable strain BA.1.";
-		}).catch(function(err) {
-		  console.error('Error downloading image:', err);
-		});
+
+		document.getElementById('img7').src = img4;
+		document.getElementById('annotation7').innerHTML = "Semantic variations and percentage rankings of mutation positions in the RBD region of the Omicron variable strain BA.1.";
+
 		
-		client.get(img5).then(function(result) {
-			var imageUrl = URL.createObjectURL(new Blob([result.content]));
-			document.getElementById('img8').src = imageUrl;
-			document.getElementById('annotation8').innerHTML = "A violin chart of semantic changes in the Omicron variable strain BA.1.";
-		}).catch(function(err) {
-		  console.error('Error downloading image:', err);
-		});
+
+		document.getElementById('img8').src = img5;
+		document.getElementById('annotation8').innerHTML = "A violin chart of semantic changes in the Omicron variable strain BA.1.";
 		
 		document.getElementById('vocimgs').style.display = 'block';
 	}
 	
 	if(selectedValue == 6){
-		var img3 = "14.png";
-		var img4 = "15.png";
-		var img5 = "16.png";
-		alert("请求已发送，请等待！")
+		var img3 = "./images/14.png";
+		var img4 = "./images/15.png";
+		var img5 = "./images/16.png";
+		alert("The request has been sent, please wait！")
 		await sleep(10000);
-		client.get(img3).then(function(result) {
-			var imageUrl = URL.createObjectURL(new Blob([result.content]));
-			document.getElementById('img6').src = imageUrl;
-			document.getElementById('annotation6').innerHTML = "The mutation location of the RBD region of the OMICRON variable strain EG.5.";
-		}).catch(function(err) {
-		  console.error('Error downloading image:', err);
-		});
+
+		document.getElementById('img6').src = img3;
+		document.getElementById('annotation6').innerHTML = "The mutation location of the RBD region of the OMICRON variable strain EG.5.";
+
 		
-		client.get(img4).then(function(result) {
-			var imageUrl = URL.createObjectURL(new Blob([result.content]));
-			document.getElementById('img7').src = imageUrl;
-			document.getElementById('annotation7').innerHTML = "Semantic variation and percentage ranking of the RBD region of the OMICRON variation strain EG.5.";
-		}).catch(function(err) {
-		  console.error('Error downloading image:', err);
-		});
+
+		document.getElementById('img7').src = img4;
+		document.getElementById('annotation7').innerHTML = "Semantic variation and percentage ranking of the RBD region of the OMICRON variation strain EG.5.";
+
 		
-		client.get(img5).then(function(result) {
-			var imageUrl = URL.createObjectURL(new Blob([result.content]));
-			document.getElementById('img8').src = imageUrl;
-			document.getElementById('annotation8').innerHTML = "A violin chart of semantic changes in the OMICRON variable strain EG.5";
-		}).catch(function(err) {
-		  console.error('Error downloading image:', err);
-		});
+
+		document.getElementById('img8').src = img5;
+		document.getElementById('annotation8').innerHTML = "A violin chart of semantic changes in the OMICRON variable strain EG.5";
+
 		document.getElementById('vocimgs').style.display = 'block';
 	}
 }
